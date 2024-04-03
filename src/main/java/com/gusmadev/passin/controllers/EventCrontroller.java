b2fdfba03d9d97df0ac76ead1c60091b8457894a
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-04-02 21:05:09 -0300

    feat: initial structure of controllers /attendees and /events

---
+++ src/main/java/com/gusmadev/passin/controllers/EventCrontroller.java
@@ -0,0 +1,9 @@
+package com.gusmadev.passin.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/envents")
+public class EventCrontroller {
+}
