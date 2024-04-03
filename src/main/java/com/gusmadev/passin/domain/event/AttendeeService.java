8e0644bba9e073afa555149a79b487bce5069165
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-04-03 16:07:32 -0300

    feat: attendee service

---
+++ src/main/java/com/gusmadev/passin/domain/event/AttendeeService.java
@@ -0,0 +1,18 @@
+package com.gusmadev.passin.domain.event;
+
+import com.gusmadev.passin.domain.attendee.Attendee;
+import com.gusmadev.passin.repositories.AttendeeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AttendeeService {
+    private AttendeeRepository attendeeRepository;
+
+    public List<Attendee> getAllAttendeesFromEvent(String eventId) {
+        return this.attendeeRepository.findByEventId(eventId);
+    }
+}
--- src/main/java/com/gusmadev/passin/services/EventSerivce.java
+++ src/main/java/com/gusmadev/passin/services/EventSerivce.java
@@ -1,12 +1,12 @@
 package com.gusmadev.passin.services;
 
 import com.gusmadev.passin.domain.attendee.Attendee;
+import com.gusmadev.passin.domain.event.AttendeeService;
 import com.gusmadev.passin.domain.event.Event;
 import com.gusmadev.passin.domain.event.exceptions.EventNotFoundException;
 import com.gusmadev.passin.dto.event.EventIdDTO;
 import com.gusmadev.passin.dto.event.EventRequestDTO;
 import com.gusmadev.passin.dto.event.EventResponseDTO;
-import com.gusmadev.passin.repositories.AttendeeRepository;
 import com.gusmadev.passin.repositories.EventRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,11 +19,11 @@ import java.util.List;
 public class EventSerivce {
 
     private final EventRepository eventRepository;
-    private final AttendeeRepository attendeeRepository;
+    private final AttendeeService attendeeService;
 
     public EventResponseDTO getEventDetail(String eventId){
         Event event = this.eventRepository.findById(eventId).orElseThrow(() -> new EventNotFoundException("Event not found with Id:" + eventId));
-        List<Attendee> attendeeList = this.attendeeRepository.findByEventId(eventId);
+        List<Attendee> attendeeList = this.attendeeService.getAllAttendeesFromEvent(eventId);
         return new EventResponseDTO(event, attendeeList.size());
     }
 
