fbe8ec580b340aa27215fa0b8d90984caf1bad8f
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-04-03 13:14:07 -0300

    feat: enpoint to create a new event

--- src/main/java/com/gusmadev/passin/controllers/EventCrontroller.java
+++ src/main/java/com/gusmadev/passin/controllers/EventCrontroller.java
@@ -1,14 +1,13 @@
 package com.gusmadev.passin.controllers;
 
 import com.gusmadev.passin.dto.event.EventIdDTO;
+import com.gusmadev.passin.dto.event.EventRequestDTO;
 import com.gusmadev.passin.dto.event.EventResponseDTO;
 import com.gusmadev.passin.services.EventSerivce;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/events")
@@ -20,5 +19,12 @@ public class EventCrontroller {
     public ResponseEntity<EventResponseDTO> getEvent(@PathVariable String eventId){
         EventResponseDTO event = this.service.getEventDetail(eventId);
         return ResponseEntity.ok(event);
+    }
+    @PostMapping
+    public ResponseEntity<EventIdDTO> createEvent(@RequestBody EventRequestDTO body, UriComponentsBuilder uriComponentsBuilder){
+        EventIdDTO eventId = this.service.createEvent(body);
+        var uri = uriComponentsBuilder.path("/events/{eventId}").buildAndExpand(eventId.eventId()).toUri();
+
+        return ResponseEntity.created(uri).body(eventId);
     }
 }
--- src/main/java/com/gusmadev/passin/dto/event/EventResponseDTO.java
+++ src/main/java/com/gusmadev/passin/dto/event/EventResponseDTO.java
@@ -1,7 +1,9 @@
 package com.gusmadev.passin.dto.event;
 
 import com.gusmadev.passin.domain.event.Event;
+import lombok.Getter;
 
+@Getter
 public class EventResponseDTO {
     EventDetailDTO event;
 
