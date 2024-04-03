4f2d8ed3a94305e3e183be81d7c32f8e737d2860
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-04-03 16:31:45 -0300

    refactor: attendee service

--- src/main/java/com/gusmadev/passin/services/AttendeeService.java
+++ src/main/java/com/gusmadev/passin/services/AttendeeService.java
@@ -1,18 +1,38 @@
 package com.gusmadev.passin.services;
 
 import com.gusmadev.passin.domain.attendee.Attendee;
+import com.gusmadev.passin.domain.chechin.CheckIn;
+import com.gusmadev.passin.dto.attendee.AttendeeDetails;
+import com.gusmadev.passin.dto.attendee.AttendeesListResponseDTO;
 import com.gusmadev.passin.repositories.AttendeeRepository;
+import com.gusmadev.passin.repositories.CheckinRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class AttendeeService {
     private AttendeeRepository attendeeRepository;
+    private CheckinRepository checkinRepository;
 
     public List<Attendee> getAllAttendeesFromEvent(String eventId) {
         return this.attendeeRepository.findByEventId(eventId);
+    }
+
+    public AttendeesListResponseDTO getEventsAttendee(String eventId){
+        List<Attendee> attendeeList = this.getAllAttendeesFromEvent(eventId);
+
+        List<AttendeeDetails> attendeeDetailsList = attendeeList.stream().map(attendee -> {
+            Optional<CheckIn> checkIn = this.checkinRepository.findByAttendeeId(attendee.getId());
+            LocalDateTime checkInAt = checkIn.isPresent() ? checkIn.get().getCreatedAt() : null;
+            return new AttendeeDetails(attendee.getId(), attendee.getName(), attendee.getEmail(), attendee.getCreatedAt(), checkInAt);
+        }).toList();
+
+        return new AttendeesListResponseDTO(attendeeDetailsList);
     }
 }
